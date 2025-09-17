85ac01dba44aa76495c00688ba115a57c27f9a1d
MELLIZA JIMENA PATERNINA RODRGUEZ <11jimena.rodriguezz03@gmail.com>
2025-09-17 10:36:01 -0500

    Initial

--- Rhombus/Program.cs
+++ Rhombus/Program.cs
@@ -1,19 +1,15 @@
-﻿using System;
-
-class Rhombus
+﻿class Rhombus
 {
     static void Main()
     {
-        while (true)
+        string input;
+        do
         {
-            Console.Write("Enter the size of the rhombus (ESC to quit): ");
-            bool esc;
-            string input = ReadLineOrEsc(out esc);
-            if (esc)
-            {
-                Console.WriteLine("Program finished.");
+            Console.Write("Enter the size of the rhombus or type 'exit' to quit: ");
+            input = Console.ReadLine()!.ToLower();
+
+            if (input == "exit")
                 break;
-            }
 
             if (!int.TryParse(input, out int size) || size <= 0)
             {
@@ -22,33 +18,10 @@ class Rhombus
             }
 
             DrawRhombus(size);
-        }
-    }
 
-    static string ReadLineOrEsc(out bool esc)
-    {
-        esc = false;
-        var sb = new System.Text.StringBuilder();
-        while (true)
-        {
-            var key = Console.ReadKey(true);
-            if (key.Key == ConsoleKey.Escape) { esc = true; Console.WriteLine(); return null; }
-            if (key.Key == ConsoleKey.Enter) { Console.WriteLine(); return sb.ToString(); }
-            if (key.Key == ConsoleKey.Backspace)
-            {
-                if (sb.Length > 0)
-                {
-                    sb.Length--;
-                    Console.Write("\b \b");
-                }
-                continue;
-            }
-            if (char.IsDigit(key.KeyChar))
-            {
-                sb.Append(key.KeyChar);
-                Console.Write(key.KeyChar);
-            }
-        }
+        } while (input != "exit");
+
+        Console.WriteLine("Program finished.");
     }
 
     static void DrawRhombus(int size)
@@ -58,6 +31,7 @@ class Rhombus
             Console.Write(new string(' ', size - i));
             Console.WriteLine(new string('#', 2 * i - 1));
         }
+
         for (int i = size - 1; i >= 1; i--)
         {
             Console.Write(new string(' ', size - i));
